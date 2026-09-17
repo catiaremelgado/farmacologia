7ddf9cb1f3e7d3fb7148f0f6ba953a30edc13be9
Cátia Remelgado <catiavanessa256@gmail.com>
2026-09-17 18:47:59 +0100

    divided

---
+++ medicamento.js
@@ -0,0 +1,28 @@
+// medicamentos.js
+const pharmaData = [
+  {
+    drug: "Amoxicilina",
+    grupo: "Anti-infeciosos",
+    subgrupo: "Antibacterianos",
+    subsubgrupo: "Penicilinas"
+  },
+  {
+    drug: "Ciprofloxacina",
+    grupo: "Anti-infeciosos",
+    subgrupo: "Antibacterianos",
+    subsubgrupo: "Fluoroquinolonas"
+  },
+  {
+    drug: "Omeprazol",
+    grupo: "Aparelho Digestivo",
+    subgrupo: "Antiácidos e Antiulcerosos",
+    subsubgrupo: "Inibidores da Bomba de Protões (IBP)"
+  },
+  {
+    drug: "Metformina",
+    grupo: "Aparelho Hormonal e Metabolismo",
+    subgrupo: "Antidiabéticos",
+    subsubgrupo: "Biguanidas"
+  }
+  // Basta ires acrescentando aqui mais fármacos com vírgula no final
+];
